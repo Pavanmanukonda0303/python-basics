fac6da223096455feb03b5cd4316801b8c2913ae
mpavank5 <mpavank5@ford.com>
2025-05-15 18:16:59 +0530

    first program

---
+++ basic_math.py
@@ -0,0 +1,15 @@
+print('Enter two numbers')
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = float(number1 / number2)
+
+print('Addition: ', addition)
+print('Subtraction: ', subtraction)
+print('Multiplication: ', multiplication)
+print('Division: ', division)
+
+
+
