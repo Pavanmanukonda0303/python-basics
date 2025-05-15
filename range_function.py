f05a9d5ee2fb31c324e5611cd503cb5a7ebce5c4
mpavank5 <mpavank5@ford.com>
2025-05-15 21:42:41 +0530

    range program

---
+++ range_function.py
@@ -0,0 +1,23 @@
+
+# The range function has 3 parameters -- range(start value, end value and step)
+
+# Example 1: only end value.
+n=list(range(10))
+print(n)
+
+# Example 2: with start and end value
+n=list(range(10,20))
+print(n)
+
+# Example 3: With  start, end value and step
+n=list(range(10,20,2))
+print(n)
+
+## Out put
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+# [10, 12, 14, 16, 18]
+
+
+
+
